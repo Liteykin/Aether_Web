03490adf66fff60b411b88e83f403cca78e1b69e
L-Mark <liteykin.mark@gmail.com>
2023-01-22 23:59:32 +0100

    Header Finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,30 +20,29 @@ const Navbar = () => {
                 <div className='aether__navbar-links_logo'>
                     <img src={logo} alt='logo' />
                 </div>
-            </div>
-            <div className='aether__navbar-links_container'>
-                <Menu />
+                <div className='aether__navbar-links_container'>
+                    <Menu />
+                </div>
             </div>
             <div className='aether__navbar-sign'>
             <p>Sign In</p>
             <button type='button'>Sign Up</button>
             </div>
-            <div className='aether__navbar-menu'>
+            <div className="aether__navbar-menu">
                 {toggleMenu
-                    ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
-                    : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
-                }
+                    ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+                    : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
                 {toggleMenu && (
-                    <div className='aether__navbar-menu_container scale-up-center'>
-                        <div className='aether__navbar-menu_container-links'>
-                            <Menu />
-                            <div className='aether__navbar-menu_container-links-sign'>
-                                <p>Sign In</p>
-                                <button type='button'>Sign Up</button>
-                            </div>
-                        </div>
+                <div className="aether__navbar-menu_container scale-up-center">
+                    <div className="aether__navbar-menu_container-links">
+                        <Menu />
                     </div>
-                    )}
+                <div className="aether__navbar-menu_container-links-sign">
+                    <p>Sign in</p>
+                    <button type="button">Sign up</button>
+                </div>
+            </div>
+            )}
             </div>
         </div>
     );
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './header.css';
 const Header = () => {
     return (
-        <div>
+        <div className='aether__header section__padding' id='home'>
+            <div className='aether__header-content'>
+                <h1 className='gradient__text'> The new Aether Bot</h1>
+                <p>Register now</p>
+            </div>
+                <input type='email' placeholder='Enter your email' />
+                <button type='button'>Get notified</button>
         </div>
     );
 };
