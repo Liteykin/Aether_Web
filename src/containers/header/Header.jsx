086fc974bea2dd0ff1afd99bb1acef283b67b0ad
L-Mark <liteykin.mark@gmail.com>
2023-01-23 01:19:14 +0100

    Header Finished

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import './header.css';
+import network from '../../assets/network.png';
 const Header = () => {
+    const onEmailSubmit = values => {
+
+    }
     return (
-        <div className='aether__header section__padding' id='home'>
-            <div className='aether__header-content'>
-                <h1 className='gradient__text'> The new Aether Bot</h1>
-                <p>Register now</p>
-            </div>
-                <input type='email' placeholder='Enter your email' />
-                <button type='button'>Get notified</button>
-        </div>
+        <div className="aether__header section__padding" id="home">
+    <div className="aether__header-content">
+      <h1 className="gradient__text">The new Aether Bot</h1>
+      <p>Register for an email</p>
+
+      <div className="aether__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+      </div>
+
+    <div className="aether__header-image">
+      <img src={network} />
+    </div>
+  </div>
     );
 };
 
